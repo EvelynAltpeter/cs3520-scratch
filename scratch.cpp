2cfa849dd9b639c6a38287dd87e67e17dce954b5
EvelynAltpeter <altpeter.e@northeastern.edu>
2025-10-01 11:38:26 -0400

    9/30

--- scratch.cpp
+++ scratch.cpp
@@ -1,61 +1,58 @@
-#Sep 29
+using namespace std;
+
+#include <iostream>
+using namespace std;
 /*
-Use gcov to determine how many times the lines commented LINE 1 and LINE 2 are executed (each).
+int main() {
+    const int size = 5;   // define grid size
+    const int wy = 3;
+    int px = 1, py = 1;
+    int cx = 2, cy = 2;
+    int tx = 4, ty = 0;
+    int wx = 3;
 
-You can paste the code into scratch.cpp from the demo to run in VS Code.
+    for (int yy = 0; yy < size; ++yy) {
+        for (int xx = 0; xx < size; ++xx) {
+            if (xx == px && yy == py) {
+                cout << '@';
+            } else if (xx == cx && yy == cy) {
+                cout << "#";
+            } else if (xx == tx && yy == ty) {
+                cout << '0';
+            } else if (xx == wx && yy == wy) {
+                cout << 'X';
+            } else {
+                cout << '_';
+            }
+        }
+        cout << endl;
+    }
+}
 
-Hint: You may need to change your Makefile.
 */
 
-/* ANSWER:
-LINE 1: 100
-LINE 2: 3363
+/*
+Write a program that prompts the user to enter an integer.  It prints out "even" or "odd" if the integer is even or odd, respectively (hint: use the mod % operator).  Then, if the integer is not negative, it prints out the square root of the integer (hint: use sqrt).  Don't worry about error handling for now.
 */
 
 #include <iostream>
+#include <cmath>
 using namespace std;
 
 int main() {
-    int x = 0;
-    int y = 0;
-    for (int ii = 0; ii < 100; ++ ii) {
-        x = ((x + 1) * 7) % 97; // LINE 1
-        for (int jj = 0; jj < x; ++ jj) {
-            y = ((y + 1) * 5) % 97; // LINE 2
-        }
+    int n;
+    cout << "Enter an integer:" << endl;
+    cin >> n;
+
+    if (n % 2 == 0) {
+        cout << "even" << endl;
+    } else {
+        cout << "odd" << endl;
     }
-    cout << x << " " << y << endl;
-}
 
-
-#include <iostream>
-using namespace std;
-
-void mystery1() {
-    int x = 0;
-    int y = 0;
-    for (int ii = 0; ii < 1000000; ++ ii) {
-        x = ((x + 1) * 7) % 97;
-        for (int jj = 0; jj < x; ++ jj) {
-            y = ((y + 1) * 5) % 97;
-        }
+    if (n >= 0) {
+        cout << "Square Root:" << sqrt(n) << endl;
     }
-    cout << x << " " << y << endl;
-}
 
-void mystery2() {
-    int x = 0;
-    int y = 0;
-    for (int ii = 0; ii < 100000; ++ ii) {
-        x = ((x + 1) * 7) % 5011;
-        for (int jj = 0; jj < x; ++ jj) {
-            y = ((y + 1) * 5) % 97;
-        }
-    }
-    cout << x << " " << y << endl;
-}
-
-int main() {
-    mystery1();
-    mystery2();
+    return 0;
 }
